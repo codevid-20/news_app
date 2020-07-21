827ff0c1eae3a441f117ff408b7aa675a13f23a6
brian <riceb53@gmail.com>
2020-07-20 22:15:23 -0400

    hide api key

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -1,6 +1,6 @@
 class Api::ArticlesController < ApplicationController
   def index
-    response = HTTP.get("http://newsapi.org/v2/everything?q=#{params[:search]}&from=2020-07-20&to=2020-07-20&sortBy=popularity&apiKey=")
+    response = HTTP.get("http://newsapi.org/v2/everything?q=#{params[:search]}&from=2020-07-20&to=2020-07-20&sortBy=popularity&apiKey=#{Rails.application.credentials.news_api}")
     @articles = response.parse
     
     render 'index.json.jb'
